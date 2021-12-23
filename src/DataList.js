619ddfb3d558eee94025b4bd8627b8ec98a77cc4
Muthusakthivel <smuthukumar1014@gmail.com>
2021-12-23 11:11:36 +0530

    updated

--- src/DataList.js
+++ src/DataList.js
@@ -2,17 +2,41 @@ import { useEffect, useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
 
 function DataList(){
     const[userList, setUserList] = useState([]);
 
+
     const columns = [
         {dataField: 'id', text: 'Id'},
-        {dataField: 'name', text: 'Name', sort: true},
-        {dataField: 'username', text: 'Username', sort: true},
-        {dataField: 'email', text: 'Email', sort: true}
+        {dataField: 'name', text: 'Name', sort: true, filter: textFilter()},
+        {dataField: 'username', text: 'Username', sort: true, filter: textFilter()},
+        {dataField: 'email', text: 'Email', sort: true, filter: textFilter()}
         
     ]
+    const pagination = paginationFactory({
+        page: 1,
+        sizePerPage: 5,
+        lastPageText: '>>',
+        firstPageText: '<<',
+        nextPageText: '>',
+        prePageText: '<',
+        showTotal: true,
+        alwaysShowAllBtns: true,
+        onPageChange: function(page, sizePerPage){
+            console.log('page', page);
+            console.log('sizerPerPage', sizePerPage);
+        },
+        onSizePerPageChange: function(page, sizerPerPage){
+            console.log('page', page);
+            console.log('sizePerPage', sizerPerPage);
+        }
+        
+    });
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -21,30 +45,14 @@ function DataList(){
     .catch(err => console.log(err));
   },[])
     return <div>
-        <BootstrapTable bootstrap4 keyField='id' columns={columns} data={userList} />
-
-       {/* <table>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Username</th>
-                <th>Email</th>
-            </tr>
-            {
-             userList && userList.length> 0 ?
-             userList.map(usr =>
-                <tr>
-                <td>{usr.id}</td>
-                <td>{usr.name}</td>
-                <td>{usr.username}</td>
-                <td>{usr.email}</td>
-            </tr>
-            
-                )
-                :'Loading'
-            
-            }
-        </table>*/}
+        <BootstrapTable 
+        bootstrap4 
+        keyField='id' 
+        columns={columns} 
+        data={userList}
+        pagination={pagination}
+        filter={filterFactory()}
+        />
     </div>
 }
 export default DataList;
